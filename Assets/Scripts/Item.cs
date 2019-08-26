94d1e31b80e392045e41c5127dc4290dc3d2c93f
jfcarocota <jfcarocota@gmail.com>
2019-08-26 11:27:51 -0700

    tile palette

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+            Destroy(gameObject);
+        }
+    }
+}
