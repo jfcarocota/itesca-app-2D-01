9bd40f3ef80e1d77fbb2386dbdccaa9104771581
jfcarocota <jfcarocota@gmail.com>
2019-09-11 22:30:44 -0700

    correciones y mejoras

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -9,6 +9,7 @@ public class NPC : MonoBehaviour
 
     protected Animator animator;
     protected SpriteRenderer spriteRenderer;
+    protected Collider2D collider2D;
 
     protected bool moving;
 
@@ -18,11 +19,14 @@ public class NPC : MonoBehaviour
     [SerializeField, Range(0.1f, 5f)]
     float minDistanceFollow;
     
+
+    Vector2 npcDirection;
     
     void Awake()
     {
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        collider2D = GetComponent<Collider2D>();
     }
 
     public virtual void Move()
@@ -30,15 +34,25 @@ public class NPC : MonoBehaviour
         moving = Vector2.Distance(leader.transform.position, transform.position) > minDistanceFollow;
         if(moving)
         {
+            npcDirection = leader.transform.position - transform.position;
+            npcDirection.Normalize();
             transform.position = Vector2.MoveTowards(transform.position, leader.transform.position, moveSpeed * Time.deltaTime);
-            animator.SetFloat("move-X", leader.Axis.x);
-            animator.SetFloat("move-Y", leader.Axis.y);
+            animator.SetFloat("move-X", npcDirection.x);
+            animator.SetFloat("move-Y", npcDirection.y);
         }
-        animator.SetBool("moving", moving);
     }
 
     protected void Update()
     {
+        animator.SetBool("moving", moving);
         spriteRenderer.flipX = leader.Axis.x < 0 ? true : leader.Axis.x > 0 ? false : spriteRenderer.flipX;
+    }
+
+    void OnCollisionEnter2D(Collision2D col2d)
+    {
+        if(col2d.collider.CompareTag("Player"))
+        {
+            Physics2D.IgnoreCollision(col2d.collider, collider2D);
+        }
     }
 }
